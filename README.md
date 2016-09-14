fa23bbc35601beba24cd1a21cd50a4c940fd5f63
Igor Shevchenko <cyberskunk@gmail.com>
2016-09-14 23:37:15 +0300

    make Express work for us

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# eact-universal-demo
+# react-universal-demo
+Based on
+https://scotch.io/tutorials/react-on-the-server-for-beginners-build-a-universal-react-and-node-app
--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-NODE_ENV=production node_modules/.bin/webpack -p
+#NODE_ENV=production node_modules/.bin/webpack -p
+NODE_ENV=production node_modules/.bin/babel-node --presets 'react,es2015' src/server.js
